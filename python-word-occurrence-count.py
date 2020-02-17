9a1e4658be9d6cee586f772b8664fcdee7c6e09c
Narendra Nauth <s.narendranauth@gmail.com>
2020-02-17 08:45:20 -0400

    Added python-word-occurrence-count program

---
+++ python-word-occurrence-count.py
@@ -0,0 +1,21 @@
+#Variable Declaration & Initialization
+text        = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy."
+wordHash    = {}
+
+#Splitting text into Array of Words
+words       = text.split(' ')
+
+#If word exist in Word Hash THEN Increment its Sount, ELSE Start its Sount at 1
+def checkForWord(search, words):
+    if search in wordHash:
+        wordHash[search] += 1
+    else:
+        wordHash[search] = 1
+
+#Iterate through Word Array to determine its Sount
+for word in words:
+    checkForWord(word, words)
+
+#Display Words and their Counts
+print(wordHash)
+    
